c64301825af5bb1bcc64078e2785ec51bbfe7910
rendhive <rendhivecode@gmail.com>
2026-07-02 00:27:35 +0700

    [DOCT] Sample Library Python: 7. Mengonfigurasi Startup.py

---
+++ 7. Mengonfigurasi Startup.py
@@ -0,0 +1,9 @@
+import site
+
+def custom_startup():
+    print("Ini adalah konfigurasi startup custom!")
+
+site.main()  # Menjalankan startup dengan berita custom
+print("Konfigurasi startup dikerjakan.")
+# Fungsi: Menghimpun dan menjalankan startup pada saat initialisation.
+# Kondisi: Ketika Anda memerlukan pengaturan awal untuk program.
