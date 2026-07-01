3ff36da661995966b4d601d7a80f543b42c35891
rendhive <rendhivecode@gmail.com>
2026-07-02 00:27:21 +0700

    [DOCT] Sample Library Python: 6. Melihat Pengaturan Modular.py

---
+++ 6. Melihat Pengaturan Modular.py
@@ -0,0 +1,6 @@
+import site
+
+print("Pengaturan awal lokasi modul:")
+print(site.getsitepackages())
+# Fungsi: Menampilkan pengaturan awal terkait lokasi modul.
+# Kondisi: Ketika Anda ingin melihat jalur lokasi penginstalan pustaka.
