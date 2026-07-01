b7e7baa22ec4ea6cb60f3de934f14da48862a29c
rendhive <rendhivecode@gmail.com>
2026-07-02 00:29:19 +0700

    [DOCT] Sample Library Python: 14. Melihat Jalur Runtuh Pustaka.py

---
+++ 14. Melihat Jalur Runtuh Pustaka.py
@@ -0,0 +1,6 @@
+import site
+
+print("Jalur lingkungan pustaka:")
+print(site.getsitepackages())
+# Fungsi: Melihat jalur tempat pustaka terinstal di sistem Anda.
+# Kondisi: Ketika Anda melakukan audit jalur pustaka dalam lingkungan pengembangan Anda.
