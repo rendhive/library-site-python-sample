5721d8c8d5c5c80b2d7e102b7cbb58d73238d91a
rendhive <rendhivecode@gmail.com>
2026-07-02 00:28:45 +0700

    [DOCT] Sample Library Python: 12. Mengatur Default untuk Modul.py

---
+++ 12. Mengatur Default untuk Modul.py
@@ -0,0 +1,6 @@
+import site
+
+site.ENABLE_USER_SITE = True
+print("User site telah diaktifkan.")
+# Fungsi: Mengatur opsi untuk mendukung directory pengguna untuk modifikasi.
+# Kondisi: Ketika Anda ingin kontrol penuh di penginstallan module melalui user.
