bd72c85940916f27c1db05e7d9d9b09b43ea3dcd
rendhive <rendhivecode@gmail.com>
2026-07-02 00:28:32 +0700

    [DOCT] Sample Library Python: 11. Melihat Informasi Kompiler.py

---
+++ 11. Melihat Informasi Kompiler.py
@@ -0,0 +1,6 @@
+import site
+
+compiler_info = site.get_config_var('COMPILER')
+print(f"Dari sistem kompilasi: {compiler_info}")
+# Fungsi: Mendapatkan informasi tentang kompilasi lingkungan.
+# Kondisi: Ketika Anda perlu memeriksa informasi tentang sistem kompilasi.
