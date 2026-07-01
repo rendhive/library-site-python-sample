48ff826f0efe3fd57a64f7c34b27f5fa01e807a1
rendhive <rendhivecode@gmail.com>
2026-07-02 00:25:58 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul site.py

---
+++ 1. Mengimpor Modul site.py
@@ -0,0 +1,5 @@
+import site
+
+print("Modul site berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk mengelola jalur modul dan direktori `site-packages`.
+# Kondisi: Digunakan sebelum memanggil fungsi atau fitur dari modul site.
