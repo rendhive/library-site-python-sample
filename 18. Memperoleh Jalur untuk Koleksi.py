cb76476b0a9e86577b745d8a526ed9ae2da8533c
rendhive <rendhivecode@gmail.com>
2026-07-02 00:30:25 +0700

    [DOCT] Sample Library Python: 18. Memperoleh Jalur untuk Koleksi.py

---
+++ 18. Memperoleh Jalur untuk Koleksi.py
@@ -0,0 +1,6 @@
+import site
+
+paths = site.getuserbase()
+print(f"Jalur untuk pustaka pengguna: {paths}")
+# Fungsi: Mendapatkan jalur untuk pustaka pengguna saat ini.
+# Kondisi: Ketika Anda ingin melihat jalur direktori di mana pustaka pengguna diinstal.
