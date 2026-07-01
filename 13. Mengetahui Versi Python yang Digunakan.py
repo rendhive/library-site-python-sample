bc39fa9a4a30ee63b4df8e9fbc4d0962df39feea
rendhive <rendhivecode@gmail.com>
2026-07-02 00:29:01 +0700

    [DOCT] Sample Library Python: 13. Mengetahui Versi Python yang Digunakan.py

---
+++ 13. Mengetahui Versi Python yang Digunakan.py
@@ -0,0 +1,6 @@
+import site
+
+python_version = site.get_python_version()
+print(f"Versi Python yang digunakan: {python_version}")
+# Fungsi: Mengambil versi Python yang sedang dijalankan.
+# Kondisi: Ketika Anda perlu memastikan apakah kode berjalan di versi tertentu.
