ccf685e833415a1fe411f883294b924786be19d5
rendhive <rendhivecode@gmail.com>
2026-07-02 00:30:41 +0700

    [DOCT] Sample Library Python: 19. Menetapkan Standar Pustaka.py

---
+++ 19. Menetapkan Standar Pustaka.py
@@ -0,0 +1,6 @@
+import site
+
+site.addsitedir('/path/to/my/libs')
+print("Direktori pustaka kustom ditambahkan.")
+# Fungsi: Mendaftar direktori pustaka kustom untuk menyertakan modul.
+# Kondisi: Ketika Anda ingin menggunakan modul dari direktori yang tidak standar.
