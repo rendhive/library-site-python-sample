14195a8c5e4b53ea54d7996daffe677a22552927
rendhive <rendhivecode@gmail.com>
2026-07-02 00:27:48 +0700

    [DOCT] Sample Library Python: 8. Direktori Home Pengguna.py

---
+++ 8. Direktori Home Pengguna.py
@@ -0,0 +1,6 @@
+import site
+
+user_site = site.USER_SITE
+print(f"Direktori site pengguna: {user_site}")
+# Fungsi: Mengambil lokasi direktori pengguna untuk paket pihak ketiga.
+# Kondisi: Ketika Anda ingin menginstal modul yang hanya tersedia untuk pengguna.
