d4ac172315bfc6f08db3147a70edcbf65bd50c74
rendhive <rendhivecode@gmail.com>
2026-07-02 00:30:08 +0700

    [DOCT] Sample Library Python: 17. Konfigurasi Proplikasi.py

---
+++ 17. Konfigurasi Proplikasi.py
@@ -0,0 +1,9 @@
+import site
+
+def configure_project():
+    print("Mengatur direktori untuk proyek...")
+    print("Setup selesai.")
+
+configure_project()
+# Fungsi: Mengatur konfigurasi direktori untuk proyek spesifik.
+# Kondisi: Ketika Anda ingin men-setting path untuk proyek tertentu di pengembangan.
