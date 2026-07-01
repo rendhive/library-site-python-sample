6e05498a8f3ff84a3d9d5baa5f161dc3866eb0b7
rendhive <rendhivecode@gmail.com>
2026-07-02 00:27:03 +0700

    [DOCT] Sample Library Python: 5. Mengambil Variabel Lingkungan Python.py

---
+++ 5. Mengambil Variabel Lingkungan Python.py
@@ -0,0 +1,6 @@
+import site
+
+python_home = site.PREFIXES
+print(f"Variabel lingkungan Python: {python_home}")
+# Fungsi: Mengambil variabel lingkungan yang digunakan saat menjalankan Python.
+# Kondisi: Ketika Anda memerlukan informasi tentang lingkup instalasi Python.
